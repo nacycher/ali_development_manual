d89ec2c2fadd4077a39a7440efbc88350df3dee7
nacycher <nacycher@163.com>
2025-01-09 10:48:30 +0800

    init project

---
+++ songshan_17que/src/main/java/org/lb/songshan_17que/Songshan17queApplication.java
@@ -0,0 +1,13 @@
+package org.lb.songshan_17que;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Songshan17queApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Songshan17queApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lb/ali_development_manual/AliDevelopmentManualApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lb.ali_development_manual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliDevelopmentManualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
